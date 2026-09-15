0bf3e7d8b1cc00e16efa4dd80077890b665d48fe
minhtam1106dn <329519234+minhtam1106dn@users.noreply.github.com>
2026-09-15 22:02:19 +0700

    Work around iOS date input padding overflow

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Increase this version when changing the offline shell or cache strategy.
 const CACHE_PREFIX = 'tkb-' + new URL(self.registration.scope).pathname + '-';
-const CACHE_NAME = CACHE_PREFIX + 'v10';
+const CACHE_NAME = CACHE_PREFIX + 'v11';
 const APP_URL = new URL('./', self.registration.scope).href;
 const ASSETS = [
   './', './index.html', './checklist.js', './auth-sync.js', './cloud-config.js', './checklist.css', './images.jpeg', './manifest.webmanifest',
